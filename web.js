75ac00d00122eb5fce53a2ee1ba68aff819f7f9d
Maserati GT <lingchuan@outlook.com>
2013-07-11 06:40:42 +0000

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = fs.readFileSync("index.html");
+  var buf = fs.readFileSync("index.html");
   response.send(buf);
   // response.send('Hello World 2!');
 });
