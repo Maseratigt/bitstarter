24666e8dd03f72503dc51b4fdb8a0d0fd3dce5ae
Maserati GT <lingchuan@outlook.com>
2013-07-12 00:56:49 +0000

    update with URL option

--- grader.js
+++ grader.js
@@ -50,6 +50,7 @@ if (require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-l --url <url>', 'URL')
 		.parse(process.argv);
 		var checkJson = checkHtmlFile(program.file, program.checks);
 		var outJson = JSON.stringify(checkJson, null, 4);
